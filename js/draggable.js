183b3e3b0ca4bae787590c858d1fc25d7e6eddca
Guillaume <gly@GLY.nounours460.mos>
2016-06-23 15:11:42 +0200

    v 5.00

--- js/draggable.js
+++ js/draggable.js
@@ -89,62 +89,29 @@ $(function() {
     });
 }
 
-// affichage des fenetres textes en fonction des logos
-$("#hockey_text,#boxhockey").click(function() {
-  if ($("#boxhockey").hasClass('close')) {
-    $("#boxhockey").removeClass('close');
-    $("#ch img,#guitar img").addClass('close');
-  } else {
-    $("#boxhockey").addClass('close');
-    $("#ch img,#guitar img").removeClass('close');
-}});
-$("#ch,#boxch").click(function() {
-  if ($("#boxch").hasClass('close')) {
-    $("#boxch").removeClass('close');
-    $("#hockey_text img,#guitar img").addClass('close');
-  } else {
-    $("#boxch").addClass('close');
-    $("#hockey_text img,#guitar img").removeClass('close');
-}});
-$("#guitar,#boxguitar").click(function() {
-  if ($("#boxguitar").hasClass('close')) {
-    $("#boxguitar").removeClass('close');
-    $("#hockey_text img,#ch img").addClass('close');
-  } else {
-    $("#boxguitar").addClass('close');
-    $("#hockey_text img,#ch img").removeClass('close');
-}});
-
-// // déplacement du gardien en fonction de la souris
-// function findPos(obj) {
-//   var curleft = curtop = 0; //Contiendra les coordonnées de obj.
-//   if (obj.offsetParent) { //Si le navigateur supporte offsetParent.
-//       do {
-//           curleft += obj.offsetLeft; //On met à jour les marges.
-//           curtop += obj.offsetTop;
-//       } while (obj = obj.offsetParent); //Si obj a un parent on le lui assigne et on continue, sinon on arrête la boucle.
-//   }
-//   return [curleft,curtop]; //On retourne la position sous forme de tableau.
-// }
-//
-// document.onmousemove = function(e) {
-//     if (!e) {
-//         var e = window.event;
-//     }
-//     var mX = parseInt(e.pageX ? e.pageX : e.clientX, 10);
-//     var mY = parseInt(e.pageY ? e.pageY : e.clientY, 10);
-//
-//     if (document.all) {
-//         mX += document.documentElement.scrollLeft;
-//         mY += document.documentElement.scrollTop;
-//     }
-//
-//     var cadre = document.getElementById("zone_but");
-//     var pos = findPos(cadre);
-//     var goal = document.getElementById("goaly");
-//
-//     mX = Math.min(Math.max(pos[0], mX - (goal.offsetWidth/2)), pos[0] + cadre.offsetWidth - goal.offsetWidth);
-//
-//     goal.style.right = mX + 'px';
-//     };
+  // affichage des fenetres textes en fonction des logos
+  $("#hockey_text,#boxhockey").click(function() {
+    if ($("#boxhockey").hasClass('close')) {
+      $("#boxhockey").removeClass('close');
+      $("#ch img,#guitar img").addClass('close');
+    } else {
+      $("#boxhockey").addClass('close');
+      $("#ch img,#guitar img").removeClass('close');
+  }});
+  $("#ch,#boxch").click(function() {
+    if ($("#boxch").hasClass('close')) {
+      $("#boxch").removeClass('close');
+      $("#hockey_text img,#guitar img").addClass('close');
+    } else {
+      $("#boxch").addClass('close');
+      $("#hockey_text img,#guitar img").removeClass('close');
+  }});
+  $("#guitar,#boxguitar").click(function() {
+    if ($("#boxguitar").hasClass('close')) {
+      $("#boxguitar").removeClass('close');
+      $("#hockey_text img,#ch img").addClass('close');
+    } else {
+      $("#boxguitar").addClass('close');
+      $("#hockey_text img,#ch img").removeClass('close');
+  }});
 });
